7c76c13a0e744b9f1595c9c3466a9491605e64dc
Vitor <vitorlimadomingues@gmail.com>
2024-09-19 11:23:09 -0300

    Criando tipos de variáveis.

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 
 //Declarando as variaveis
 
-
-
 var nome="fiap";
 console.log(nome)
 
@@ -23,3 +21,28 @@ console.log(nome3)
 let disciplina =null;
 console.log(disciplina)
 
+//Tipos de variaveis
+
+//Inteiro
+
+let idade = 18;
+console.log(idade)
+
+//Flutuantes ou com casas decimais
+
+let valor= 6.40;
+console.log(valor)
+
+//String (textos)
+
+let texto="Fiap";
+console.log(texto)
+
+//Booleanos - verdadeiro ou falso
+
+let aluno=true
+console.log(aluno)
+
+//Array - conjunto de informações
+let tecnologias =["JAVA", "C#", "HTML", "CSS"];
+console.log(tecnologias)
